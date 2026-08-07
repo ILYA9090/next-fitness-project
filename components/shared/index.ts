ca8be1ea1efc6ac2d4c72af79ff9afbad0420fd4
Ilya Ovchinnikov <ovchina900@mail.ru>
2026-08-07 18:04:47 +0400

    FEAT: add checkbox filters

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -2,3 +2,7 @@ export { Container } from "./container";
 export { Header } from "./header";
 export { Title } from "./title";
 export { Categories } from "./categories";
+export { SortPopup } from "./sortPopup";
+export { TopBar } from "./topBar";
+export { Filters } from "./filters";
+export { CheckboxFilter } from "./checkboxFilter";
